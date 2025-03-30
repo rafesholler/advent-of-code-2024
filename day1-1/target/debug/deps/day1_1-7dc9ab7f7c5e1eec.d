7e32fe48e49b3f5b2fcc6b1477f244888c3a9fbb
rafesholler <rafe.sholler@gmail.com>
2025-03-29 23:57:13 -0400

    Finished day 1

--- day1-1/target/debug/deps/day1_1-7dc9ab7f7c5e1eec.d
+++
@@ -1,5 +0,0 @@
-/home/rafes/Documents/Programming/Rust/advent-of-code-2024/day1-1/target/debug/deps/libday1_1-7dc9ab7f7c5e1eec.rmeta: src/main.rs
-
-/home/rafes/Documents/Programming/Rust/advent-of-code-2024/day1-1/target/debug/deps/day1_1-7dc9ab7f7c5e1eec.d: src/main.rs
-
-src/main.rs:
